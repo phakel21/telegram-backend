692d3bb985079d992feb244a5e90342d3949c12d
phakel <viktor@gmail.com>
2024-08-11 13:13:40 +0200

    s

--- bot.js
+++ bot.js
@@ -29,20 +29,20 @@ const { Markup, Telegraf } = require("telegraf");
 const TOKEN = "7186227610:AAFINi141u4RKGRGJssIRAUDnuYxrDFOakM";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://velvety-lollipop-e867de.netlify.app/";
+const web_link = "https://phakel21.github.io/";
 
-// bot.start((ctx) =>
-//   ctx.reply("Welcome :)))))", {
-//     reply_markup: {
-//       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-//     },
-//   })
-// );
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
 
-// bot.launch();
-
-
-bot.start(ctx => {
-  return ctx.reply(`Hello ${ctx.update.message.date}!`);
-});
 bot.launch();
+
+
+// bot.start(ctx => {
+//   return ctx.reply(`Hello ${ctx.update.message.date}!`);
+// });
+// bot.launch();
